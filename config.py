01b454e3937773ff21fbff2bf8fa880546d0f5a0
Joyce Johnson <joyce.johnson513@gmail.com>
2025-05-31 12:57:28 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
-# sockpuppet_agent/config.py
-
 CONFIG = {
     "openai_api_key": "YOUR_OPENAI_KEY_HERE",
     "fastmail_username": "your_fastmail_user",
-    "fastmail_password": "your_fastmail_app_password",
+    # This should be your Fastmail app password (token), not your regular password!
+    "fastmail_api_token": "your_fastmail_app_password",
+    "fastmail_api_base_url": "https://api.fastmail.com/jmap",
+    "fastmail_domain": "fastmail.com",
     "use_local_llm": True,
     "local_llm_endpoint": "http://localhost:11434/api/generate",
     "use_tor_proxy": True,
