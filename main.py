75f7ae4e8d4b35fe64eb2fd5f8b5294ce0af9896
Joyce Johnson <joyce.johnson513@gmail.com>
2025-05-31 19:07:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from agents.puppet_creator import generate_sock_puppet
 
 if __name__ == "__main__":
-    puppet = generate_sock_puppet(
-        use_openai=True,
-        use_local_llm=True,
-        use_proxy=True
-    )
+    puppet = generate_sock_puppet()
+    
     print("\nGenerated Sock Puppet:")
     print(puppet)
